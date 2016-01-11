2ba02a301c30707256e66c2252f973028ffbee4a
KostasG <kostasgan@e-food.gr>
2016-01-11 15:59:52 +0200

    login changes

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -6,19 +6,15 @@ export default Ember.Controller.extend({
 		check: function(){
 			var user= this.store.peekRecord('user',1);
 
-			//alert(this.get(username));
-
 			if((this.get('email') === user.get("username")) && (this.get('pass') === user.get("password"))){
-				user.set('id',0);
+				
+
 				this.transitionToRoute("main");
 			}
 			else{
-				user.set('logged', true),
-				console.log(user.logged),
+				user.set('logged', true);
 				Ember.$('#error').fadeOut(1500);
-
-			}
-			
+			}	
 		}
 	}
 });
