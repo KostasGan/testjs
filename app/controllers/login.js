be92ddab210a9b63c249367881dfb41bfd705291
KostasG <kostasgan@e-food.gr>
2016-01-12 11:41:08 +0200

    login changes

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,20 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({	
-	logged:false,
+	
 	actions:{
 		check: function(){
 			var user= this.store.peekRecord('user',1);
 
 			if((this.get('email') === user.get("username")) && (this.get('pass') === user.get("password"))){
-				
-
 				this.transitionToRoute("main");
 			}
 			else{
-				user.set('logged', true);
+				user.set('logged',true);
 				Ember.$('#error').fadeOut(1500);
-				alert('yolo');
+
 			}
 	
 		}
