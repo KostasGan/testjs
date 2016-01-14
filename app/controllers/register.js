8c448c0f31e81be4aded26187a9c506d2a50d8ba
KostasG <kostasgan@e-food.gr>
2016-01-14 17:18:42 +0200

    register controller

---
+++ app/controllers/register.js
@@ -0,0 +1,32 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+		isSame:false,
+
+		actions:{
+		
+			register:function(){
+
+				if(this.get('pass') === this.get('confirm')){
+					this.store.createRecord('user',{
+						id:'2',
+						fullname: this.get('fullname'),
+						username: this.get('username'),
+						email: this.get('email'),
+						password: this.get('pass'),
+						logged: true
+					})
+					//newUser.save();
+					this.set('isSame',false);
+					this.transitionToRoute("login");
+
+				}
+				else{
+					this.set('isSame',true);
+					Ember.$('#error').fadeOut(1500);
+				}
+
+			},			
+		}
+
+});
