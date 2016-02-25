3ff26523b7f2287e7e1f2fc970e41c4787020e06
KostasG <kostasgan@e-food.gr>
2016-02-25 18:34:39 +0200

    all new files for main/contact

---
+++ app/controllers/main/contact.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+	isValidEmail: Ember.computed.notEmpty('contactMail'),
+	
+	isMessageMaxChars: Ember.computed.gte('contactMessage.length', 5),
+
+	FormValidated: Ember.computed.and('isValidEmail','isMessageMaxChars'),
+
+	isNotValid: Ember.computed.not('FormValidated')
+});
