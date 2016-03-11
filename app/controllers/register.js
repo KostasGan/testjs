c4c4fc4832ced223cb8f66db86fa41a3699048eb
KostasG <kostasgan@e-food.gr>
2016-03-11 16:41:12 +0200

    delete some spaces and tabs

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -1,33 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-		isSame:false,
+	isSame:false,
+	actions:{
+		register:function(){
 
-		actions:{
-		
-			register:function(){
-
-				if(this.get('pass') === this.get('confirm')){
-					var newUser=this.store.createRecord('user',{
-						id:'2',
-						fullname: this.get('fullname'),
-						username: this.get('username'),
-						email: this.get('email'),
-						password: this.get('pass'),
-						logged: false
-					})
+			if(this.get('pass') === this.get('confirm')){
+				var newUser=this.store.createRecord('user',{
+					id:'2',
+					fullname: this.get('fullname'),
+					username: this.get('username'),
+					email: this.get('email'),
+					password: this.get('pass'),
+					logged: false
+				});
 					
-					newUser.save();
-					this.set('isSame',false);
-					this.transitionToRoute("login");
-
-				}
-				else{
-					this.set('isSame',true);
-					Ember.$('#error').fadeOut(1500);
-				}
-
-			},			
-		}
+				newUser.save();
+				this.set('isSame',false);
+				this.transitionToRoute("login");
+			}
+			else{
+				this.set('isSame',true);
+				Ember.$('#error').fadeOut(1500);
+			}
+		}			
+	}
 
 });
