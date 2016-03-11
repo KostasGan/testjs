5f8586e4fa0db6a3f11b87a3b6da294cf186d423
KostasG <kostasgan@e-food.gr>
2016-03-11 15:04:01 +0200

    set userid as functions parameter + console logs

--- app/services/retrieve.js
+++ app/services/retrieve.js
@@ -2,7 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
 	CurrentUser:null,
-	retrievedUser:function(user){
-		this.set('CurrentUser',user);	
-	}
+	store:Ember.inject.service(),
+
+	retrievedUser(userid){
+		var user = this.get('store').find('user', userid);
+		this.set('CurrentUser',user);
+		Cookies.set('userID', userid);
+		console.log("No Cookie exist! Retrieved data with login");	
+	},
+
+	logoutUser(userid){
+		this.set('CurrentUser',null);
+		Cookies.remove('userID',userid);
+		console.log("Data and Cookies deleted with logout..Transition to login");
+	},
+
+	initializeFromCookie:function(){
+		var userID= Cookies.get('userID');
+		if(!!userID){
+			var user = this.get('store').find('user', userID);
+			this.set('CurrentUser',user);
+			console.log('Cookies exist! Sessions data retrieved');	
+		}
+	}.on('init')
 });
