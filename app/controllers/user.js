4828707221b8b03d7980c79e88fbdf2558a0feb8
KostasG <kostasgan@e-food.gr>
2016-03-11 16:42:04 +0200

    add signout action in user profile

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	actions:{
+		signout:function(){
+			this.get('sessionUser').logoutUser(this.get('session.uid'));
+			this.transitionToRoute('login');
+		}
+	}
 });
