642b9a60aad6807b1d995f59fa870b4bf7f0604c
Kostas Gan <kostasgan@e-food.gr>
2016-03-12 21:31:38 +0200

    functionality for edit a library properties

--- app/routes/libraries/edit.js
+++ app/routes/libraries/edit.js
@@ -1,5 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	
+	model(params) {
+    	return this.store.peekRecord('library', params.library_id);
+  	},
+
+ 	actions: {
+    	createLibrary(newLibrary) {
+      		newLibrary.save().then(() => this.transitionTo('libraries'));
+    	},
+
+    	willTransition(transition) {
+    		
+      		let model = this.controller.get('model');
+
+      		if (model.get('hasDirtyAttributes')) {
+        		let confirmation = confirm("Your changes haven't saved yet. Would you like to leave this form?");
+
+        		if (confirmation) {
+          			model.rollbackAttributes();
+       			} else {
+          			transition.abort();
+        		}
+      		}
+    	}
+  	}	
 });
